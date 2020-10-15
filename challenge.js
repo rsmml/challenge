c932fff53d33356c2a3b1d6c808adb3ebf6c91c6
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-10-15 12:54:11 +0200

    readme file added

--- challenge.js
+++ challenge.js
@@ -1,52 +1,55 @@
-const url = 'https://join.dev.neta.sh/api/interview-tests/vault-of-sweets';
-const bearer = 'Bearer '+ `64?`;
+const getHTMLTags = () => {
+  const HTMLTags = document.getElementsByTagName('*');
+  //1. Get all tagNames in an Array;
+  const allHTMLTags = Array.prototype.slice.call(HTMLTags);
 
-fetch(url, {
-  method:"GET",
-  mode: "no-cors",
-  headers:{
-    'Authorization': bearer,
-    // "Access-Control-Allow-Origin": "*",
-    "Candidate-Email": "rodrigosommacal@gmail.com",
-  },
-})
-.then(response => response.text())
-.then((data) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(data, 'text/html');
-  const HTMLTags = doc.getElementsByTagName('*');
+  //2. Adding tagNames as String in array;
+  const tagNames = [];
 
-//1. Get all tagNames in an Array;
-const allHTMLTags = Array.prototype.slice.call(HTMLTags);
+  tagNamesToString = (tagName) => { tagNames.push(tagName.tagName); }
+  allHTMLTags.forEach(tagNamesToString);
 
-//2. Adding tagNames as String in array;
-const tagNames = [];
+  //3. Creating Object Key = tagName, Value = how many times it repeats;
+  const tagNamesCounter = {};
+  tagNames.forEach((i) => {tagNamesCounter[i] = (tagNamesCounter[i] || 0) + 1; });
 
-tagNamesToString = (tagName) => {
-  tagNames.push(tagName.tagName);
+  //4. Sorting Object;
+  const sortedTagNames = {};
+  Object.keys(tagNamesCounter).sort().forEach((key) => { sortedTagNames[key] = tagNamesCounter[key] });
+
+  //5. Adding tags to array as pair 'keyvalue' + join them lowercase;
+  const arrayOfTagNames = [];
+  Object.entries(sortedTagNames).forEach(([k,v]) => { arrayOfTagNames.push(`${k}${v}`)});
+
+  const secretKey = arrayOfTagNames.join('');
+  return secretKey
 }
-allHTMLTags.forEach(tagNamesToString);
 
-//3. Creating Object Key = tagName, Value = how many times it repeats;
-const tagNamesCounter = {};
+const uncodeSecretKey = () => {
+  const token = getHTMLTags();
+  // const key = 'A12BODY1BR8DIV38FOOTER1H12H22H31H44H51HEAD1HTML1IMG1LI7LINK4META4NAV1P8SCRIPT1SECTION3STYLE1TITLE1UL2' // from DOM https://join.dev.neta.sh/api/interview-tests/vault-of-sweets;
+  const uncodeSecretKey = atob(token);
+  return uncodeSecretKey;
+}
 
-tagNames.forEach((i) => {tagNamesCounter[i] = (tagNamesCounter[i] || 0) + 1; });
+const accessEndPoint = () => {
+  const url = 'https://join.dev.neta.sh/api/interview-tests/vault-of-sweets';
+  const bearer = 'Bearer '+ `${uncodeSecretKey()}`;
 
-//4. Sorting Object;
-const sortedTagNames = {};
-
-Object.keys(tagNamesCounter).sort().forEach((key) => { sortedTagNames[key] = tagNamesCounter[key] });
-
-//5. Adding tags to array as pair 'keyvalue' + join them lowercase;
-const arrayOfTagNames = [];
-Object.entries(sortedTagNames).forEach(([k,v]) => { arrayOfTagNames.push(`${k}${v}`)});
-
-const secretKey = arrayOfTagNames.join('').toLocaleLowerCase();
-console.log(secretKey);
-
-const result = document.getElementById('result');
-result.insertAdjacentHTML('afterend', `<p>${secretKey}</p>`);
-
-});
-
-
+  fetch(url, {
+    method:'GET',
+    mode: 'no-cors',
+    headers:{
+      'Authorization': bearer,
+      'Candidate-Email': 'rodrigosommacal@gmail.com',
+    },
+  })
+    .then(response => response.text())
+    .then((data) => {
+      console.log(data);
+  })
+    .catch(err => {
+      console.error(err.message);
+  });
+}
+accessEndPoint();
